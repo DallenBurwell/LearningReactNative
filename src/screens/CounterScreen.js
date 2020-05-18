c7592bdfd9433091e89105cbc7c65d860971fecb
Dallen <dallen.burwell@gmail.com>
2020-05-17 22:41:43 -0600

    Replaced useState with useReducer in counter and square demos

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+    return {...state, counter: state.counter + action.payload };
+}
+
 const CounterScreen = () => {
-    const[counter, setCounter] = useState(0);
-    
+    const[state, dispatch] = useReducer(reducer, { counter: 0 });
+    const { counter } = state;
     
     return (
         <View>
             <Button 
                 title='Increase'
                 onPress={() => {
-                    setCounter(counter + 1);
+                   dispatch({ type: 'change_increase', payload: 1});
                 }}
             />
             <Button 
                 title='Decrease'
                 onPress={() => {
-                    setCounter(counter - 1);
+                    dispatch({ type: 'change_decrease', payload: -1});
                 }}
             />
             <Text>Current Count: {counter}</Text>
